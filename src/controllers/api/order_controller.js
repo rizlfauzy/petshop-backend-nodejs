d03698cc31d53ae7018e164b4c057c8dc9cfd58a
Rizal Fauzi <rizalfauzi774@gmail.com>
2024-07-14 18:17:06 +0700

    [FEATURE] print invoice pembelian

--- src/controllers/api/order_controller.js
+++ src/controllers/api/order_controller.js
@@ -6,6 +6,13 @@ import generate_kode from "../../utils/generate_kode";
 import month_diff from "../../utils/month_diff";
 import moment from "moment-timezone";
 import sq from "../../db";
+import fs from "fs";
+import { options_pdf } from "../../utils/options";
+import pdf from "pdf-creator-node";
+import { format_rupiah, deformat_rupiah } from "../../utils/format";
+import compile_hbs from "../../utils/compile_hbs";
+require("dotenv").config();
+const { APP_URL } = process.env;
 moment.tz.setDefault("Asia/Jakarta");
 
 const order_cont = {
@@ -21,6 +28,38 @@ const order_cont = {
     } catch (e) {
       await transaction.rollback();
       return res.status(500).json({ message: e.message });
+    }
+  },
+  print: async (req, res) => {
+    const transaction = await sq.transaction();
+    const path_file = "./public/pdf/";
+    try {
+      const { nomor } = req.query;
+      const name_file = "penerimaan-barang-" + nomor + "-" + moment().format("YYYYMMDDHHmmss") + ".pdf";
+      const data = await order.findOne({ attributes: ["nomor", "tanggal", "keterangan"], where: { nomor } }, { transaction });
+      const detail = await cari_pembelian.findAll({ attributes: ["barcode", "nama_barang", "qty", "harga", "total"], where: { nomor } }, { transaction });
+      const data_detail = JSON.parse(JSON.stringify(detail)).map((item, i) => ({ ...item, harga: format_rupiah(item.harga), total: format_rupiah(item.total), no: i + 1 }));
+
+      data.setDataValue("list_barang", data_detail);
+      data.setDataValue("waktu_cetak", moment().format("HH:mm:ss"));
+      data.setDataValue("grand_total", format_rupiah(data_detail.reduce((acc, curr) => acc + Number(deformat_rupiah(curr.total)), 0)));
+      if (!fs.existsSync("./public/")) fs.mkdirSync("./public/");
+      if (!fs.existsSync(path_file)) fs.mkdirSync(path_file);
+      const html = compile_hbs("invoice_pembelian", { order: data.toJSON() });
+      const document = {
+        html,
+        data: { order: data.toJSON() },
+        path: path_file + name_file,
+      };
+      await pdf.create(document, options_pdf);
+      await transaction.commit();
+      setTimeout(() => {
+        fs.unlinkSync(path_file + name_file);
+      }, 1500);
+      return res.status(200).json({ data, message: "Data berhasil didapatkan !!!", error: false, url: `${APP_URL}/pdf/${name_file}` });
+    } catch (e) {
+      await transaction.rollback();
+      return res.status(500).json({ message: e.message, error: true, data });
     }
   },
   save: async (req, res) => {
@@ -66,22 +105,20 @@ const order_cont = {
               { where: { barcode: barang.barcode, periode } }
             );
           } else {
-            await inventory_barang.create(
-              {
-                periode,
-                barcode: barang.barcode,
-                qty_awal: 0,
-                qty_masuk: Number(barang.qty),
-                qty_keluar: 0,
-                pemakai: req.user.myusername.toUpperCase(),
-                tglsimpan: moment().format("YYYY-MM-DD HH:mm:ss"),
-              }
-            );
+            await inventory_barang.create({
+              periode,
+              barcode: barang.barcode,
+              qty_awal: 0,
+              qty_masuk: Number(barang.qty),
+              qty_keluar: 0,
+              pemakai: req.user.myusername.toUpperCase(),
+              tglsimpan: moment().format("YYYY-MM-DD HH:mm:ss"),
+            });
           }
         }
       }
 
-      await order.create({ nomor, tanggal: moment(tanggal).format("YYYY-MM-DD"), keterangan, tglsimpan: moment().format("YYYY-MM-DD HH:mm:ss"), pemakai: req.user.myusername.toUpperCase() }, { transaction });
+      await order.create({ nomor, tanggal: moment(tanggal).format("YYYY-MM-DD"), keterangan: keterangan.toUpperCase(), tglsimpan: moment().format("YYYY-MM-DD HH:mm:ss"), pemakai: req.user.myusername.toUpperCase() }, { transaction });
 
       for (const barang of list_barang) {
         const { barcode, qty, harga, total_harga: total } = barang;
@@ -183,7 +220,7 @@ const order_cont = {
       }
 
       await order.update(
-        { tanggal: moment(tanggal).tz("Asia/Jakarta").format("YYYY-MM-DD"), keterangan, tglupdate: moment().tz("Asia/Jakarta").format("YYYY-MM-DD HH:mm:ss"), pemakai: req.user.myusername.toUpperCase() },
+        { tanggal: moment(tanggal).tz("Asia/Jakarta").format("YYYY-MM-DD"), keterangan: keterangan.toUpperCase(), tglupdate: moment().tz("Asia/Jakarta").format("YYYY-MM-DD HH:mm:ss"), pemakai: req.user.myusername.toUpperCase() },
         { where: { nomor }, transaction }
       );
 
